beeba65454c77d42f57b0b65fa1f488c86c7bd9d
Cem <cemsalih91@gmail.com>
2018-11-17 10:56:18 +0000

    Removing automated stub

--- src/Run.java
+++ src/Run.java
@@ -2,7 +2,6 @@
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Board board = new Board();
 		Frame frame = new Frame(board);
 		TicTacToe tictactoe = new TicTacToe(frame, board);
